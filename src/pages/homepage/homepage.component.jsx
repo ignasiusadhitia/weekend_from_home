5513809e2ec47f3761241cec62f3c8be6a6f6883
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-04-29 21:55:26 +0700

    feat: hero section layouting

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../../layouts/hero-section/hero-section.component";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
