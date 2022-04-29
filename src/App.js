e440f94b9803737a14bf0fc89bb524dd6cc48443
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-04-29 16:11:45 +0700

    project initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assets/scss/default.scss";
+import astronout1 from "./assets/images/astronout-1.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEEKEND FROM HOME</h1>
+      <img src={astronout1} alt="astronout" />
+    </div>
+  );
+}
+
+export default App;
