1274591ae3134ef0ee9e38f429edeacf9865b799
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-04-30 21:43:46 +0700

    feat: created quote-section layout, arranged astronout and light red shape background

---
+++ src/layouts/qoute-section/quote-section.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./quote-section.styles.scss";
+
+const QuoteSection = () => {
+  return (
+    <div className="quote-section-container">
+      <div className="light-red-background-container-2"></div>
+      <div className="astronout-container-2"></div>
+    </div>
+  );
+};
+
+export default QuoteSection;
