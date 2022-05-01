505c36df57d82d0ebf8c441b4789764a27121671
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-05-01 13:48:20 +0700

    fix: added project styling default setting and hero section re-layouting

--- src/layouts/hero-section/hero-section.component.jsx
+++ src/layouts/hero-section/hero-section.component.jsx
@@ -6,16 +6,29 @@ import "./hero-section.styles.scss";
 const HeroSection = () => {
   return (
     <div className="hero-container">
-      <div className="background-wrapper"></div>
+      <div className="background main"></div>
       <div className="heading-wrapper">
-        <h1>WEEKEND FROM HOME</h1>
-        <h3>Stay active with a little workout.</h3>
+        <h1 className="p">WEEKEND FROM HOME</h1>
+        <p className="p semi-bold-italic">Stay active with a little workout.</p>
       </div>
-      <div className="astronout-wrapper"></div>
+      <div className="background astronout-1"></div>
       <CustomButton buttonText="Let's Go" className="custom-button" />
-      <div className="light-red-background-wrapper"></div>
-      <div className="astronout-wrapper-2"></div>
-      <div className="quote-wrapper"></div>
+      <div className="background light-red-1"></div>
+      <div className="background astronout-2"></div>
+
+      <div className="paragraph-wrapper quote-wrapper">
+        <div className="paragraph-width medium">
+          <p className="text-black">
+            <span className="bold text-blue">Deffinition;</span> a practice or
+            exercise to test or improve one's fitness for athletic competition,
+            ability, or performance to exhaust (something, such as a mine) by
+            working to devise, arrange, or achieve by resolving difficulties.
+            Merriam-Webster.com Dictionary.
+          </p>
+          <p className="semi-bold-italic quote-author">-weekend team</p>
+        </div>
+      </div>
+      <div className="background oval"></div>
     </div>
   );
 };
