7e22182a0afb07df302fd97f6f90d9253b3a00aa
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-05-01 15:10:26 +0700

    fix: header and footer contents

--- src/layouts/footer/footer.component.jsx
+++ src/layouts/footer/footer.component.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./footer.styles.scss";
 
 const Footer = () => {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="max-width-1 footer">
+        <span className="footer-copyright">wknd@2020</span>
+        <span className="footer-version">alpha version 0.1</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
