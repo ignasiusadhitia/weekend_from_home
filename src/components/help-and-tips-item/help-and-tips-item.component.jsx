0bc8b6720bf87466b1f677f54d1a4be8d5be4396
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-05-03 12:37:06 +0700

    feat: created help-and-tips-item component; fix: help-and-tips-section layout

---
+++ src/components/help-and-tips-item/help-and-tips-item.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./help-and-tips-item.styles.scss";
+
+const HelpAndTipsItem = () => {
+  return <div className="help-and-tips-item-container">HelpAndTipsItem</div>;
+};
+
+export default HelpAndTipsItem;
